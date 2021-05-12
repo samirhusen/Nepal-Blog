50038a6457f1a4a33972f9f47da334ecf901a967
samirhusen <saw.mendes.meer@gmail.com>
2021-05-12 10:50:07 +0545

    Completed

---
+++ Nepal Blog/assets/js/main.js
@@ -0,0 +1,25 @@
+(function($) {
+
+	var	$window = $(window),
+		$banner = $('#banner'),
+		$body = $('body');
+
+	// Breakpoints
+		breakpoints({
+				default:   ['1681px',   null       ],
+					xlarge:    ['1281px',   '1680px'   ],
+					large:     ['981px',    '1280px'   ],
+			medium:    ['737px',    '980px'    ],
+					small:     ['481px',    '736px'    ],
+					xsmall:    ['361px',    '480px'    ],
+			xxsmall:   [null,       '360px'    ]
+		});
+
+	// Play initial animations
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+})(jQuery);
